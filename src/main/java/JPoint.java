f57e62e6a83755e5573231d533c6789589148e6e
Vladimir Bauer <VVBauer.SBT@sberbank.ru>
2014-08-19 14:37:22 +0400

    r test

--- src/main/java/JPoint.java
+++ src/main/java/JPoint.java
@@ -4,5 +4,11 @@
 public class JPoint {
     public static void main(String[] args) {
         System.out.println(9/10*10);
+        System.out.println("Random: " + someRandom());
+    }
+
+
+    public static int someRandom(){
+        return (int) (Math.random() * 10);
     }
 }
